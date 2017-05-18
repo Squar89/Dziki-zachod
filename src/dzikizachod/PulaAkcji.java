797e9bbd4a96eab5c0f2f7d68f1449c3f0c24009
Jakub Wróblewski <jw386401@students.mimuw.edu.pl>
2017-05-18 18:53:23 +0200

    implemented PulaAkcji class

--- src/dzikizachod/PulaAkcji.java
+++ src/dzikizachod/PulaAkcji.java
@@ -1,9 +1,40 @@
 package dzikizachod;
 
+import java.util.ArrayList;
+import java.util.Collections;
 /**
  *
  * @author squar
  */
 public class PulaAkcji {
+    private ArrayList<Akcja> pulaKarty;
+    private ArrayList<Akcja> użyteKarty;
     
+    public PulaAkcji() {
+        this.pulaKarty = new ArrayList<>();
+        this.użyteKarty = new ArrayList<>();
+    }
+    
+    public void dodaj(Akcja akcja, int liczba) {
+        while (liczba > 0) {
+            this.pulaKarty.add(akcja);
+            
+            liczba--;
+        }
+    }
+    
+    public Akcja podajKartę() {
+        if (this.pulaKarty.isEmpty()) {
+            this.pulaKarty = this.użyteKarty;//to mi da referencje na ten Arraylist ktory jest w srodku czy na użyte karty?
+            this.użyteKarty = new ArrayList<>();
+            
+            this.przetasujPulę();
+        }
+        
+        return pulaKarty.remove(0);
+    }
+    
+    public void przetasujPulę() {
+        Collections.shuffle(this.pulaKarty);
+    }
 }
